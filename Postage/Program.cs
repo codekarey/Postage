a0d16c7aeefaf8b5062e64f5fed9883dcf9c9e84
codekarey <codekarey@gmail.com>
2020-02-27 23:48:33 -0500

    User input requests

--- Postage/Program.cs
+++ Postage/Program.cs
@@ -9,16 +9,19 @@ namespace Postage
             Console.WriteLine("USPS Mailing & Shipping Prices");
 
             //Info: Stamps / Priority / Express / First-Class / Global
+            //Provide list of common costs to reference in calculating costs
 
             //Type: Postcard, letter, envelope, package
+            //Ask for type
 
             //Distance / Method: Local, global, airmail
+            //Ask for distance and/or additional requirements
 
-            //Weight
+            //Weight if envelope=<
 
-            //Cost
+            //Calculate cost and provide links to confirm
 
-            //Returns
+            //Return information
         }
     }
 }
