d77cb1c26b98ae05ce99257a91dbcb336efad4a4
codekarey <codekarey@gmail.com>
2020-02-27 23:37:31 -0500

    Concept

--- Postage/Program.cs
+++ Postage/Program.cs
@@ -6,7 +6,19 @@ namespace Postage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("USPS Mailing & Shipping Prices");
+
+            //Info: Stamps / Priority / Express / First-Class / Global
+
+            //Type: Postcard, letter, envelope, package
+
+            //Distance / Method: Local, global, airmail
+
+            //Weight
+
+            //Cost
+
+            //Returns
         }
     }
 }
