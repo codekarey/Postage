63a5a95548319b9644add959950936e96b0741eb
codekarey <codekarey@gmail.com>
2020-03-09 12:21:17 -0400

    Mail item stores constants: size, shape, and weight

---
+++ Postage/Mail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Postage
+{
+    class Mail
+    {
+        public decimal Length { get; set; }
+        public decimal Height { get; set; }
+        public decimal Thick { get; set; }
+        public decimal Weight { get; set; }
+
+        public Mail(decimal length, decimal height, decimal thick, decimal weight)
+        {
+            Length = length;
+            Height = height;
+            Thick = thick;
+            Weight = weight;
+        }
+
+    }
+}
--- Postage/Program.cs
+++ Postage/Program.cs
@@ -7,21 +7,40 @@ namespace Postage
         static void Main(string[] args)
         {
             Console.WriteLine("USPS Mailing & Shipping Prices");
+            bool loop = true;
+            while (loop)
+            {
+                string type = Get("What will you be mailing?\n" +
+                    "1 - Enter Size & Shape   2  - Size and Shape Information");
+                switch (type)
+                {
+                    case "1":
+                        decimal h = decimal.Parse(Get("Enter the length"));
+                        decimal l = decimal.Parse(Get("Enter the height"));
+                        decimal t = decimal.Parse(Get("Enter the thickness"));
+                        decimal w = decimal.Parse(Get("Enter the weight"));
+                        Mail mailItem =  new Mail(h, l, t, w);
 
-            //Info: Stamps / Priority / Express / First-Class / Global
-            //Provide list of common costs to reference in calculating costs
-
-            //Type: Postcard, letter, envelope, package
-            //Ask for type
-
-            //Distance / Method: Local, global, airmail
-            //Ask for distance and/or additional requirements
-
-            //Weight if envelope=<
-
-            //Calculate cost and provide links to confirm
-
-            //Return information
+                        break;
+                    case "2":
+                        Console.WriteLine("These details define the shape and size of mailpiece options:\n" +
+                            "POSTCARD : Rectangular & (At least: 3-1/2 inches high x 5 inches long x 0.007 inch thick) - (No more: than 4 - 1 / 4 inches high x 6 inches long x 0.016 inches thick)\n" +
+                            "LETTER : Rectangular (At least: 3 - 1 / 2 inches high x 5 inches long x 0.007 inch thick.) - (No more than 6 - 1 / 8 inches high x 11 - 1 / 2 inches long x 1 / 4 inch thick.)\n" +
+                            "ENVELOPE : (Have one dimension that is greater than: 6 - 1 / 8 inches high OR 11 -½ inches long OR ¼ inch thick.)&(Be no more than: 12 inches high x 15 inches long x ¾ inch thick.)\n" +
+                            "PARCEL : Maximum weight is 70 pounds. (At least: 3 inches high x 6 inches long x 1 / 4 inch thick.) - (Less than: 108 inches in length and girth combined.Length is the measurement of the longest dimension and girth is the distance around the thickest part(perpendicular to the length))\n" +
+                            "If you need more information check out : https://www.usps.com/business/prices.htm");
+                       break;
+                    default:
+                        Console.WriteLine("Please enter the #");
+                        break;
+                }
+            }
         }
+        public static string Get(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+
     }
 }
