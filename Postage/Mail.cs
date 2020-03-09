de918c294123c129524e7f7cf1ed3ad04e053e3d
codekarey <codekarey@gmail.com>
2020-03-09 15:01:39 -0400

    Mail type class drafted to use after given shape and size.

--- Postage/Mail.cs
+++ Postage/Mail.cs
@@ -19,5 +19,28 @@ namespace Postage
             Weight = weight;
         }
 
+        public static string MailType(Mail item) // w > t > h/l
+        {
+            string type;
+            if (item.Weight < 70 && item.Height >= 3 && item.Height<=17 && item.Length >= 6 && item.Length<= 27 && item.Thick >= 1 / 4 && item.Thick<=17)
+            {
+                return type = "parcel";
+            }
+            if (item.Height<12 && item.Length<15 && item.Thick<3/4 &&(item.Height>6+1/8 || item.Length > 11+1/2 || item.Thick> 1 / 4))
+            {
+
+                    return type = "enevelope";
+            }
+            if (item.Height >= 3 + 1 / 2 && item.Height <= 6 + 1 / 8 && item.Length >= 5 && item.Length < 11 + 1 / 2 && item.Thick <= 1 / 4)
+            {
+                return type = "letter";
+            }
+            if(item.Height>= 3+1/2 && item.Height <=4+1/2 && item.Length>=5 && item.Length<= 6 && item.Thick>= 7/1000 && item.Thick>=16/1000 && item.Thick <= 1 / 4)
+            {
+                return type = "postcard";
+            }
+
+            return "No matching type";
+        }
     }
 }
--- Postage/Program.cs
+++ Postage/Program.cs
@@ -6,33 +6,43 @@ namespace Postage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("USPS Mailing & Shipping Prices");
+            Console.WriteLine("USPS Mailing & Shipping Prices for Priority Mail");
             bool loop = true;
             while (loop)
-            {
-                string type = Get("What will you be mailing?\n" +
-                    "1 - Enter Size & Shape   2  - Size and Shape Information");
-                switch (type)
+            {                      
+                string menu = Get("What would you like to do?\n" +
+                "1 - Enter Size & Shape   2  - Size and Shape Information");
+                try
                 {
-                    case "1":
-                        decimal h = decimal.Parse(Get("Enter the length"));
-                        decimal l = decimal.Parse(Get("Enter the height"));
-                        decimal t = decimal.Parse(Get("Enter the thickness"));
-                        decimal w = decimal.Parse(Get("Enter the weight"));
-                        Mail mailItem =  new Mail(h, l, t, w);
+                    if (menu == "1")
+                    {
+                        Console.WriteLine("Please enter values in inches and pounds. \n" +
+                           "Example: 3.5 (for length/height in inches) ~ .007 (standard thickness of an index card in inches) ~ 2.85 (for weight in pounds)\n");
+                        decimal h = decimal.Parse(Get("Enter the length in inches:"));
+                        decimal l = decimal.Parse(Get("Enter the height in inches:"));
+                        decimal t = decimal.Parse(Get("Enter the thickness in inches:"));
+                        decimal w = decimal.Parse(Get("Enter the weight in pounds:"));
+                        Mail mailItem = new Mail(h, l, t, w);
+                        string type = Mail.MailType(mailItem);
+                        Console.WriteLine("Thank you, the mail item you have is " + type);
 
-                        break;
-                    case "2":
-                        Console.WriteLine("These details define the shape and size of mailpiece options:\n" +
+                    }
+                    else if (menu == "2")
+                    {
+                             Console.WriteLine("These details define the shape and size of mailpiece options:\n" +
                             "POSTCARD : Rectangular & (At least: 3-1/2 inches high x 5 inches long x 0.007 inch thick) - (No more: than 4 - 1 / 4 inches high x 6 inches long x 0.016 inches thick)\n" +
                             "LETTER : Rectangular (At least: 3 - 1 / 2 inches high x 5 inches long x 0.007 inch thick.) - (No more than 6 - 1 / 8 inches high x 11 - 1 / 2 inches long x 1 / 4 inch thick.)\n" +
                             "ENVELOPE : (Have one dimension that is greater than: 6 - 1 / 8 inches high OR 11 -½ inches long OR ¼ inch thick.)&(Be no more than: 12 inches high x 15 inches long x ¾ inch thick.)\n" +
                             "PARCEL : Maximum weight is 70 pounds. (At least: 3 inches high x 6 inches long x 1 / 4 inch thick.) - (Less than: 108 inches in length and girth combined.Length is the measurement of the longest dimension and girth is the distance around the thickest part(perpendicular to the length))\n" +
                             "If you need more information check out : https://www.usps.com/business/prices.htm");
-                       break;
-                    default:
-                        Console.WriteLine("Please enter the #");
-                        break;
+                    }
+                }
+                catch{
+                        Console.WriteLine("If you'd like to exit enter X OR any other key to run again.");
+                    if (Console.ReadLine().ToLower() == "x")
+                    {
+                        loop = false;
+                    }
                 }
             }
         }
